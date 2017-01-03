b8d4c749571732369b0c013d013932a35f538c2d
SchwannK <schwann.khaw@gmail.com>
2017-01-03 15:42:50 +0000

    Updated Subscription cards, model, and improved UI

--- sendasubscription/urls.py
+++ sendasubscription/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('subscriptions_dir.urls')),
     url(r'^contact/', views.contact, name='contact')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- subscriptions_dir/views.py
+++ subscriptions_dir/views.py
@@ -6,6 +6,6 @@ from subscriptions_dir.utils import subscription_filter_utils
 
 def subscriptions_dir(request):
     subscriptions = Subscription.objects.order_by('company__name')
-    categories = subscription_filter_utils.get_subscription_categories(Subscription.objects.all())
+    subcategories = subscription_filter_utils.get_subscription_subcategories(Subscription.objects.all())
 
-    return render(request, 'subscriptions_dir/subscriptions_dir.html', {'navbar':'subscriptions', 'subscriptions': subscriptions, 'categories': categories,})
+    return render(request, 'subscriptions_dir/subscriptions_dir.html', {'navbar':'subscriptions', 'subscriptions': subscriptions, 'subcategories': subcategories,})
