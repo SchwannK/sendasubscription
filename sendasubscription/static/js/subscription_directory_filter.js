a31a45c9eb5da7c109a90f23b3522f31434f3f69
SchwannK <schwann.khaw@gmail.com>
2017-01-05 10:35:28 +0000

    Made sidebar sticky

--- sendasubscription/static/js/subscription_directory_filter.js
+++ sendasubscription/static/js/subscription_directory_filter.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    $("#filtersidebar").stick_in_parent({offset_top: 70});
+
     $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
 
