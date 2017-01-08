e63ed797c8bb3a5d3a1e44dbb805f1a839115ecb
Schwannk <schwann.khaw@gmail.com>
2017-01-08 02:23:21 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	sendasubscription/templates/site_base.html

--- sendasubscription/static/js/price_slider.js
+++ sendasubscription/static/js/price_slider.js
@@ -1,10 +1,24 @@
 var slider = document.getElementById('slider');
 
 noUiSlider.create(slider, {
-	start: [20, 80],
+	start: [0, 100],
+	step: 20,
 	connect: true,
+	tooltips: [wNumb({ decimals: 0 }),  wNumb({ decimals: 0 }) ],
 	range: {
 		'min': 0,
-		'max': 100
+		'max': 400
 	}
+});
+
+slider.noUiSlider.on('change', function( values, handle ) {
+
+	$(".filter").filter(function() {
+		return parseFloat($(this).attr("price")) >= values[0] && parseFloat($(this).attr("price")) <= values[1];
+	}).show('1000');
+
+	$(".filter").filter(function() {
+		return parseFloat($(this).attr("price")) < values[0] || parseFloat($(this).attr("price")) > values[1];
+	}).hide('1000');
+
 });
